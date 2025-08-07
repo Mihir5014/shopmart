a68228d1ddd5cf5e05b73e86cfbb048e4fe63b62
Mihir5014 <mihirparmar738@gmail.com>
2025-08-07 12:02:44 +0530

    ProductCard Component

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,26 @@
+type Props = {
+    product: {
+        id: number;
+        title: string;
+        image: string;
+        price: number;
+    }
+};
+
+export default function ProductCard({ product }: Props) {
+    return (
+        <div
+            className="bg-gray-900 rounded-lg shadow-md p-4 hover:shadow-lg cursor-pointer">
+
+            <div className="w-full h-60 flex items-center justify-center rounded mb-4">
+                <img src={product.image}
+                    alt={product.title}
+                    className="max-h-full object-contai" />
+            </div>
+
+            <h3 className="text-sm font-medium truncate mb-2">{product.title}</h3>
+
+            <p className="text-red-600 font-semibold text-lg">${product.price}</p>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 type Product = {
   id: number;
@@ -18,24 +20,13 @@ export default async function Home() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {
           products.map((product) => (
-            <div
-              key={product.id}
-              className="bg-gray-900 rounded-lg shadow-md p-4 hover:shadow-lg cursor-pointer">
-
-              <div className="w-full h-60 flex items-center justify-center rounded mb-4">
-                <img src={product.image}
-                  alt={product.title}
-                  className="max-h-full object-contai" />
-              </div>
-
-              <h3 className="text-sm font-medium truncate mb-2">{product.title}</h3>
-
-              <p className="text-blue-600 font-semibold text-lg">${product.price}</p>
-            </div>
-
+            <Link key={product.id} href={`/products/${product.id}`}>
+              <ProductCard product={product} />
+            </Link>
           ))
         }
-      </div></div>
+      </div>
+    </div>
   );
 }
 
